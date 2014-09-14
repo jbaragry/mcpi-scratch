6ba72b92c0e414ad2706595b766a78d6573534e4
jbaragry <jason.baragry@gmail.com>
2014-09-14 21:47:49 +0200

    first commit with basic project

---
+++ README.md
@@ -0,0 +1,71 @@
+MCPI-Scratch
+================
+
+In progress.....
+================
+
+A Scratch 2.0 extension application for the Minecraft Pi Protocol
+
+# Introduction
+MCPI-Scratch lets you write Scratch scripts to manipulate Minecraft. 
+
+More specifically, its an extension application for the Scratch 2 standalone editor that allows you to use blocks that represent the [Minecraft Pi Protocol](http://wiki.vg/Minecraft_Pi_Protocol) API. It actually uses a slightly cut-down version of the API specified by the [RaspberryJuice](http://dev.bukkit.org/bukkit-plugins/raspberryjuice/) mod which allows it to connect to both:
+* Minecraft Pi Edition
+* CraftBukkit server with the RaspberryJuice mod
+
+A more detailed description of how it works is provided in [this blog entry]()
+
+You might also want to have a look at the documentation at ScriptCraft which has a nice [The Young Person's Guide to Programming in Minecraft](https://github.com/walterhiggins/ScriptCraft/blob/master/docs/YoungPersonsGuideToProgrammingMinecraft.md). It has lots of general tips, for example on how to set up a flat world in CraftBukkit, before getting into its own javascript specific API. 
+
+You will also want to keep this [reference for block IDs and metadata]() handy.
+
+Everything is (c) Jason Baragry licensed under CC-BY-SA (Creative commons by attribution)
+
+To make it easier for Scrachers to get up and going, I've chosen to include the Minecraft Pi Edition API in this distribution. I don't think it violates the [Minecraft EULA](https://account.mojang.com/documents/minecraft_eula) but it may be that I need to remove it later and you add it yourself.
+
+The API is the one from [Minecraft Pi Edition](http://minecraft.gamepedia.com/Pi_Edition) and not the modded API from RaspberryJuice. But it might be worth changing to that one later if it gives extra functionality.
+
+The last interesting feature is how the app deals with polling from Scratch. Scratch polls for changed player position (and other reporter variables) about 30 times per second. This polling frequency can't be changed and if the app sent a corresponding `getPlayerPos` request everytime it would choke the `minecraft` server. Instead the app keeps a counter and sends the `getPlayerPos` command every 15 times - approx every 0.5 seconds.
+
+# Installation
+Download and install the [Scratch 2 offline editor](http://scratch.mit.edu/scratch2download/).
+
+You need to run this extension application on the same machine as Scratch. Scratch will then communicate with it via HTTP on port X
+
+Minecraft can either be runnnig on the same machine or on another machine which you can specify as a startup parameter. MCPI-Scratch expects Minecraft to be listening on the default port for the Minecraft Pi Protocol - `4711`
+
+MCPI-Scratch takes an optional command-line argument for the host of the minecraft server or minecraft pi 
+
+# Getting Started
+Start the applications in the following order:
+
+1. Start Minecraft. 
+This must be running before you start MCPI-Scratch because the application attempts to create a connection on startup and will fail if Minecraft is not running. You can use either:
+	* Minecraft Pi Edition or 
+	* CraftBukkit server with the RaspberryJuice mod + plus the Minecraft launcher to start a client to connect to the server
+		* There's a nice blog about setting up this env at [StuffAboutCode](http://www.stuffaboutcode.com/2013/06/programming-minecraft-with-bukkit.html)
+
+2. Start MCPI-Scratch
+The application attempts to connect to localhost by default. Use the `-m host` cmd-line arg to specify minecraft running on another machine 
+
+3. Start Scratch. 
+When the editor loads, shift-click the File menu and select `load experimental...` and select the file `filename` in the MCPI-Scratch install directory. You should now be able to select the `More Blocks` 
+
+
+# Changelog
+
+V0.1:
+* first release to test the idea
+* includes API operations
+	* getPlayerPos
+	* setPlayerPos
+	* setBlock
+		* setBlock has a switch to interpret the coordinates as being either absolute or relative to the player position
+	* setBlocks
+	* postToChat
+
+# Issues
+
+
+
+
---
+++ mcpi-scratch.py
@@ -0,0 +1,136 @@
+# simple httpserver to act as a Scratch2 helper app
+# adapted from : http://pymotw.com/2/BaseHTTPServer/
+# add python minecraft examples from http://www.stuffaboutcode.com/
+import sys, traceback
+import argparse
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import logging
+
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+class GetHandler(BaseHTTPRequestHandler):
+
+    def setBlock(self, params): # doesn't support metadata
+        print ('setblock: {0}'.format(params))
+        x = int(params[0])
+        y = int(params[1])
+        z = int(params[2])
+        if (params[5] == 'rel'): # set the block relative to the player
+            playerPos = mc.player.getPos()
+            playerPos = minecraft.Vec3(int(playerPos.x), int(playerPos.y), int(playerPos.z))
+            x += playerPos.x
+            y += playerPos.y
+            z += playerPos.z
+        if (int(params[4]) == -1): # sure these is a more pythonesque way of doing this
+            mc.setBlock(x, y, z, int(params[3]))
+        else:
+            mc.setBlock(x, y, z, int(params[3]), int(params[4]))
+        return ''
+
+    def setBlocks(self, params): # doesn't support metadata
+        log.info('invoke setBlocks with params: {} {} {} {} {} {} {} {}'.format(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7]))
+        if (int(params[7]) == -1): # sure these is a more pythonesque way of doing this
+            log.debug('invoking without data')
+            mc.setBlocks(int(params[0]), int(params[1]), int(params[2]), int(params[3]), int(params[4]), int(params[5]), int(params[6]))
+        else:
+            log.debug('invoking with data')
+            mc.setBlocks(int(params[0]), int(params[1]), int(params[2]), int(params[3]), int(params[4]), int(params[5]), int(params[6]), int(params[7]))
+        return ''        
+
+    def setPlayerPos(self, params): # doesn't support metadata
+        log.info('invoke setPlayerPos with params: {} {} {}'.format(params[0], params[1], params[2]))
+        mc.player.setPos(int(params[0]), int(params[1]), int(params[2]))
+        return ''
+
+    def postToChat(self, params):
+        log.info('post to chat: %s', params[0])
+        mc.postToChat(params[0])
+        return ''
+
+    def cross_domain(self, params):
+        # not needed for offline editor, only online webeditor
+        log.info('need to return cross_domain.xml') # needed for scratch Flash issues
+        return ''
+
+    def reset_all(self, params):
+        log.info('trying to reset')
+        return ''
+
+    def pollEvents(self, params):
+        global pollInc, pollLimit, prevPosStr
+        pollInc += 1
+        log.debug('poll: {} {}'.format(pollInc, prevPosStr))
+        if (prevPosStr != "") and (pollInc % pollLimit != 0):
+            log.debug("don't call mc")
+            return prevPosStr
+        log.debug("call mc")
+        playerPos = mc.player.getPos()
+        #Using your players position
+        # - the players position is an x,y,z coordinate of floats (e.g. 23.59,12.00,-45.32)
+        # - in order to use the players position in other commands we need integers (e.g. 23,12,-45)
+        # - so round the players position
+        # - the Vec3 object is part of the minecraft class library
+        playerPos = minecraft.Vec3(int(playerPos.x), int(playerPos.y), int(playerPos.z))
+        posStr = ("playerPos/x {0}\r\nplayerPos/y {1}\r\nplayerPos/z {2}".format(str(playerPos.x), str(playerPos.y), str(playerPos.z)))
+        prevPosStr = posStr
+        return posStr
+    
+    def do_GET(self):
+        global mc
+        cmds = {
+            "poll" : self.pollEvents,
+            "postToChat" : self.postToChat,
+            "setBlock" : self.setBlock,
+            "setBlocks" : self.setBlocks,
+            "setPlayerPos" : self.setPlayerPos,
+            "cross_domain.xml" : self.cross_domain,
+            "reset_all" : self.reset_all,
+        }
+        parsed_path = urlparse.urlparse(self.path)
+        message_parts = []
+        message_parts.append('')
+        cmdpath = parsed_path[2].split('/')
+        handler = cmds[cmdpath[1]]
+        pollResp = handler(cmdpath[2:])
+        log.debug ("pollResp: {0}".format(pollResp))
+        message_parts.append(pollResp)
+        message = '\r\n'.join(message_parts)
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(message)
+        return
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='scratch2MinecraftPi is a Scratch2 extension helper app to allow Scratch programs to manipulate Minecraft through the Pi protocol')
+    parser.add_argument('-m', action="store", dest="host", help="hostname/IP for the machine running Minecraft. Default is localhost")
+    #parser.add_argument('-p', action="store", dest="port", type=int, help="port for the machine running Minecraft with the Pi protocol enabled. Default is 4711")
+    args = parser.parse_args()
+    log.debug(args)
+
+    # nasty stuff to slow down the polling that comes from scratch.
+    # from the docs, sratch is polling at 30 times per second
+    # updating the player pos that often is causing the app to choke.
+    # use these vars to only make the call to MC every pollMax times - 15
+    # maybe change this to a cmd-line switch
+    pollInc = 0
+    pollLimit = 15
+    prevPosStr = ""
+
+    try:
+        mc = minecraft.Minecraft.create()
+    except:
+        e = sys.exc_info()[0]
+        log.exception('cannot connect to minecraft')
+        traceback.print_exc(file=sys.stdout)
+        sys.exit(0)
+
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('localhost', 4715), GetHandler)
+    log.info('Starting server, use <Ctrl-C> to stop')
+    server.serve_forever()
