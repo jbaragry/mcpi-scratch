b40750b1e16d1d26b2cb56536e183eb45eaa7a4b
jbaragry <jason.baragry@gmail.com>
2015-06-29 21:15:05 +0200

    posttochat

--- mcpi-scratch.js
+++ mcpi-scratch.js
@@ -1,16 +1,12 @@
-(function(ext) {
+(function (ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        return { status:2, msg:'Ready' };
     };
 
-    var mcpiHlprURL = "http://localhost:4715"
-
-    ext.chat_msg = function() {
+    ext.postToChat = function() {
         $.ajax({
             type: "GET",
             url: "http://localhost:4715/postToChat/Bob"
@@ -26,11 +22,11 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            // Block type, block name, function name
-            [' ', 'post to chat', 'postToChat', 'chat_msg']
+            ['', 'post to chat', 'postToChat']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('MCPI-ScratchX extension', descriptor, ext);
+    ScratchExtensions.register('MCPI-Scratch', descriptor, ext);
+
 })({});
