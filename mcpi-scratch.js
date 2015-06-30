107ac9deb673221263f66f4fdddc46670afd9ba6
jbaragry <jason.baragry@gmail.com>
2015-06-30 22:29:37 +0200

    setBlock

--- mcpi-scratch.js
+++ mcpi-scratch.js
@@ -51,13 +51,32 @@
         }); // nb: GET is including the javascript callback. Do I need this for one-way call?
     };
 
+    ext.setBlock = function(x, y, z, blockType, blockData, posType) {
+        var cmdUrl = "http://localhost:4715/setPlayerPos/" + x + "/" + y + "/" + z + "/" + blockType + "/" + blockData + "/" + posType;
+        $.ajax({
+            type: "GET",
+            url: cmdUrl,
+            dataType: "jsonp", // hack for the not origin problem - replace with CORS based solution
+            success: function(data) {
+                console.log("setBlock success");
+            },
+            error: function(jqxhr, textStatus, error) { // have to change this coz jasonp parse error
+                console.log("Error setBlock: ", error);
+            }
+        }); // nb: GET is including the javascript callback. Do I need this for one-way call?
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['', 'post to chat %s', 'postToChat', 'message'],
             [" ", "post Player.pos chat", "playerPosToChat"],
             [" ", "set Player pos to x:%n y:%n z:%n", "setPlayerPos", 0, 0, 0],
-        ]
+            [" ", "set block pos x:%n y:%n z:%n to type %n data %n %m.blockPos", "setBlock", 0, 0, 0, 1, -1],
+        ],
+        menus: {
+            blockPos: ['abs', 'rel']
+        }
     };
 
     // Register the extension
