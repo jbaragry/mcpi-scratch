370b22a6833805b1f9266d38e01b299539dc17e2
jbaragry <jason.baragry@gmail.com>
2015-06-30 23:02:05 +0200

    setBlocks, line, and circle

--- mcpi-scratch.js
+++ mcpi-scratch.js
@@ -66,6 +66,55 @@
         }); // nb: GET is including the javascript callback. Do I need this for one-way call?
     };
 
+    ext.setBlocks = function(x1, y1, z1, x2, y2, z2, blockType, blockData) {
+        var cmdUrl = "http://localhost:4715/setBlocks/" + x1 + "/" + y1 + "/" + z1 + "/" 
+            + x2 + "/" + y2 + "/" + z2 + "/" + blockType + "/" + blockData;
+        $.ajax({
+            type: "GET",
+            url: cmdUrl,
+            dataType: "jsonp", // hack for the not origin problem - replace with CORS based solution
+            success: function(data) {
+                console.log("setBlock success");
+            },
+            error: function(jqxhr, textStatus, error) { // have to change this coz jasonp parse error
+                console.log("Error setBlock: ", error);
+            }
+        }); // nb: GET is including the javascript callback. Do I need this for one-way call?
+    };
+
+    ext.setLine = function(x1, z1, x2, z2, y, blockType, blockData) {
+        var cmdUrl = "http://localhost:4715/setLine/" + x1 + "/" + z1 + "/" 
+            + x2 + "/" + z2 + "/" + y + "/" + blockType + "/" + blockData;
+        $.ajax({
+            type: "GET",
+            url: cmdUrl,
+            dataType: "jsonp", // hack for the not origin problem - replace with CORS based solution
+            success: function(data) {
+                console.log("setBlock success");
+            },
+            error: function(jqxhr, textStatus, error) { // have to change this coz jasonp parse error
+                console.log("Error setBlock: ", error);
+            }
+        }); // nb: GET is including the javascript callback. Do I need this for one-way call?
+    };
+
+    ext.setCircle = function(x, z, r, y, blockType, blockData) {
+        var cmdUrl = "http://localhost:4715/setCircle/" + x + "/" + z + "/" 
+            + r + "/" + y + "/" + blockType + "/" + blockData;
+        $.ajax({
+            type: "GET",
+            url: cmdUrl,
+            dataType: "jsonp", // hack for the not origin problem - replace with CORS based solution
+            success: function(data) {
+                console.log("setBlock success");
+            },
+            error: function(jqxhr, textStatus, error) { // have to change this coz jasonp parse error
+                console.log("Error setBlock: ", error);
+            }
+        }); // nb: GET is including the javascript callback. Do I need this for one-way call?
+    };
+
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
@@ -73,6 +122,9 @@
             [" ", "post Player.pos chat", "playerPosToChat"],
             [" ", "set Player pos to x:%n y:%n z:%n", "setPlayerPos", 0, 0, 0],
             [" ", "set block pos x:%n y:%n z:%n to type %n data %n %m.blockPos", "setBlock", 0, 0, 0, 1, -1],
+            [" ", "set blocks pos x1:%n y1:%n z1:%n to x2:%n y2:%n z2:%n to type %n data %n", "setBlocks", 0, 0, 0, 0, 0, 0, 1, -1],
+            [" ", "set line pos x1:%n z1:%n to x2:%n z2:%n height y:%n to type %n data %n", "setLine", 0, 0, 0, 0, 0, 1, -1],
+            [" ", "set circle center x1:%n z1:%n radius r:%n at height y:%n to type %n data %n", "setCircle", 0, 0, 0, 0, 0, 1, -1],
         ],
         menus: {
             blockPos: ['abs', 'rel']
